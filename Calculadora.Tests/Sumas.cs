c85fe2c09277008c556fbe5e072af87f473eaf71
Kenneth Hidalgo <kenneth52mm@hotmail.com>
2016-05-28 09:56:48 -0600

    Pruebas Unitarias

---
+++ Calculadora.Tests/Sumas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logica.Negocio;
+
+namespace Calculadora.Tests
+{
+    [TestClass]
+    public class Sumas
+    {
+        [TestMethod]
+        public void SumarUnoMasDos()
+        {
+            var primerOperando = 1;
+            var segundoOperando = 2;
+            var resultadoEsperado = 3;
+
+            var laSuma = new Suma();
+            var resultado = laSuma.Operar(primerOperando, segundoOperando);
+            Assert.AreEqual(resultadoEsperado, resultado);
+
+        }
+    }
+}
